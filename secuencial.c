3f7097be4b562cf30dfb73cf63857f995a9413f8
Matias Fuentes <matifuentes14@gmail.com>
2015-05-19 00:32:54 -0300

    first commit

---
+++ secuencial.c
@@ -0,0 +1,155 @@
+#include <stdio.h>
+#include <malloc.h>
+#include <stdlib.h>
+#include <math.h>
+
+//Para calcular tiempo
+double dwalltime(){
+	double sec;
+	struct timeval tv;
+	gettimeofday(&tv,NULL);
+	sec = tv.tv_sec + tv.tv_usec/1000000.0;
+	return sec;
+}
+
+double pow (double base , double exponent);
+float powf (float base  , float exponent);
+long double powl (long double base, long double exponent);
+
+void imprimeMatriz(double *S,int N, int order){
+	int i,j,I,J,despB;
+
+	printf("Contenido de la matriz: \n" );
+	for (i=0; i<N; i++){
+		for(j=0;j<N;j++){
+			if (order == 1){
+				printf("%f ",S[i*N+j]);
+			}
+			else {
+				printf("%f ",S[j*N+i]);
+			}
+		}
+		printf("\n ");
+	};
+	printf(" \n\n");
+}
+
+int main(int argc,char*argv[]){
+	double *A,*B,*C,*AB,*D;
+	int i,j,k,N;
+	int check=1;
+	double timetick;
+
+	if (argc < 2){
+		printf("\n Falta un argumento:: N dimension de la matriz \n");
+		return 0;
+	}
+
+	N=atoi(argv[1]);
+
+   //Aloca memoria para las matrices
+	A=(double*)malloc(sizeof(double)*N*N);
+	B=(double*)malloc(sizeof(double)*N*N);
+	C=(double*)malloc(sizeof(double)*N*N);
+	D=(double*)malloc(sizeof(double)*N*N);
+
+   //Inicializa las matrices A y B en 1, C en diagonal
+	for(i=0;i<N;i++){
+		for(j=0;j<N;j++){
+			A[i*N+j] = rand()%10;
+			B[j*N+i] = rand()%10;
+			if (i == j){
+				D[i*N+j] = rand()%10;
+			}
+			else {
+				D[i*N+j] = 0;
+			}
+		}
+	}
+
+	// imprimeMatriz(A,N,1);
+	// imprimeMatriz(B,N,0);
+	// imprimeMatriz(D,N,1);
+
+	// ********************************************
+	// *************** ETAPA 1 ********************
+	// ********************************************
+
+	timetick = dwalltime();
+
+   for(i=0;i<N;i++){
+      for(j=0;j<N;j++){
+         C[i*N+j] = 0;
+         for(k=0;k<N;k++){
+            C[i*N+j] += A[i*N+k] * B[j*N+k];
+         }
+         C[i*N+j] = C[i*N+j] * D[j*N+j]; 
+      }
+   }   
+
+	// imprimeMatriz(C,N,1);
+	printf("Tiempo en segundos de multiplicacion de matrices: %f\n", dwalltime() - timetick);  
+
+	// ********************************************
+	// *************** ETAPA 2 ********************
+	// ********************************************
+
+	int maxA,minA,maxB,minB,totA,totB;
+	double factor, avgA, avgB;
+	maxA = -1;
+	maxB = -1;
+	minB = 99999;
+	minA = 99999;
+	totA = 0;
+	totB = 0;
+
+	timetick = dwalltime();
+
+	for(i=0;i<N;i++){
+      for(j=0;j<N;j++){
+         if (A[i*N+j] > maxA){
+         	maxA = A[i*N+j];
+         } 
+         if (A[i*N+j] < minA){
+         	minA = A[i*N+j];
+         }
+         if (B[j*N+i] > maxB){
+         	maxB = B[j*N+i];
+         } 
+         if (B[j*N+i] < minB){
+         	minB = B[j*N+i];
+         }
+         totA += A[i*N+j];
+         totB += B[j*N+i];
+      }
+   }
+
+   avgA = (double) totA / (N * N);
+   avgB = (double) totB / (N * N);
+   double a = maxA - minA;
+   double b = maxB - minB;
+   factor = ((a*a)/avgA) * ((b*b)/avgB);
+
+	for(i=0;i<N;i++){
+      for(j=0;j<N;j++){
+      	C[i*N+j] = C[i*N+j] * factor;
+      }
+   }
+
+   printf("Tiempo en segundos de multiplicacion por factor: %f\n", dwalltime() - timetick);  
+
+  	//imprimeMatriz(C,N,1);
+
+	// ********************************************
+	// *************** ETAPA 3 ********************
+	// ********************************************
+
+   
+
+
+	free(A);
+	free(B);
+	free(C);
+	free(D);
+	return(0);
+}
