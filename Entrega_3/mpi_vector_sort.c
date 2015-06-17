0521929ca8892c99612efae1ccaf4728a4c2281a
Matias Fuentes <matifuentes14@gmail.com>
2015-06-17 01:04:15 -0300

    Compilando

--- Entrega_3/mpi_vector_sort.c
+++ Entrega_3/mpi_vector_sort.c
@@ -1,11 +1,10 @@
-#include <algorithm>
-#include <vector>
-#include "mpi.h" 
-#include <iostream>
-using namespace std;
+#include <malloc.h>
+#include <math.h>
+#include <stdio.h>
+#include <mpi.h>
  
 
-void merge(double *col,double *col_res, int inicio, int mid, int final)
+void merge(int *col,int *col_res, int inicio, int mid, int final)
 {
     int i = inicio, j = mid + 1, k = inicio;
     while (i <= mid && j <= final)
@@ -39,11 +38,31 @@ void merge(double *col,double *col_res, int inicio, int mid, int final)
     }
 }
 
+void sort(int *C, int N){
+    int l,j;
+    for (l=0;l<N-1;l++){
+        for(j=0;j<N-l;j++){
+            if (C[j] < C[j+1]){
+                int temp = C[j];
+                C[j] = C[j+1];
+                C[j+1] = temp;
+            }
+        }
+    }
+}
+
+void save(int *B, int from, int to, int *A){
+    int i;
+    for (i=from; i<= to; i++){
+        B[i] = A[i];
+    }
+}
+
 int main(int argc, char *argv[]) 
 { 
-    int rank, size, N;
-    int *A;
-    int *lcl;
+    int rank, size, N, i;
+    int *A, *B;
+    int *lcl, *tmp;
  
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD,&rank);
@@ -68,28 +87,29 @@ int main(int argc, char *argv[])
     }
 
     lcl = (int*)malloc(sizeof(double)*(N/size)*2);
-    bool ok = true;
-    int tag, step, out;
-    out=0; step=0;
+    tmp = (int*)malloc(sizeof(double)*(N/size)*2);
+    int ok = 1;
+    int tag, step, out, done;
+    out=0; step=0; done=0;
     //Comienza el ordenado
-    while(ok)
+    while(ok == 1)
     {
         if(rank != 0){ // Cada uno de los workers
             MPI_Request request;
             MPI_Irecv(&(lcl[0]),N/size*2,MPI_INT,0,tag,MPI_COMM_WORLD,&request); 
             MPI_Send(&(lcl[0]),0,MPI_INT,0,-1,MPI_COMM_WORLD);
             MPI_Status status; 
-            int MPI_Wait(&request, &status);
+            MPI_Wait(&request, &status);
             if (tag < size){
-                sortArray(&lcl, 0, N/size);
+                sort(lcl, N/size);
                 MPI_Send(&(lcl[0]),N/size,MPI_INT,0,tag,MPI_COMM_WORLD);
             }
             else if (tag >= size && tag < (size + size/2)){
-                merge(&lcl, 0, N/size - 1, N/size *2 - 1);
+                merge(lcl,tmp,0, N/size - 1, (N/size * 2) - 1);
                 MPI_Send(&(lcl[0]),N/size*2,MPI_INT,0,tag,MPI_COMM_WORLD);
             }
             else {
-                 ok = false;
+                 ok = 0;
             }
         }
         else // master
@@ -97,30 +117,33 @@ int main(int argc, char *argv[])
             MPI_Status status;
             MPI_Recv(&(lcl[0]),N/size*2,MPI_INT,MPI_ANY_SOURCE,tag,MPI_COMM_WORLD,&status);
             if (tag == -1){
+                MPI_Request request;
                 if (step < size){
-                    MPI_Isend(&(A[step*(N/size)]),N/size,MPI_INT,status.MPI_SOURCE,step,MPI_COMM_WORLD);
+                    MPI_Isend(&(A[step*(N/size)]),N/size,MPI_INT,status.MPI_SOURCE,step,MPI_COMM_WORLD,&request);
                     step = step + 1;
                 }
-                else if {
-                    if (step < (size + size/2)){
-                        MPI_Isend(&(A[step_2*(N/size*2)]),N/size*2,MPI_INT,status.MPI_SOURCE,step,MPI_COMM_WORLD);
-                        step_2 = step_2 + 1;
-                    }
+                else if (step < (size + size/2)){
+                    MPI_Isend(&(A[step*(N/size*2)]),N/size*2,MPI_INT,status.MPI_SOURCE,step,MPI_COMM_WORLD,&request);
+                    step = step + 1;
                 }
                 else {
-                    MPI_Isend(&(lcl[0]),0,MPI_INT,status.MPI_SOURCE,step,MPI_COMM_WORLD);
+                    MPI_Isend(&(lcl[0]),0,MPI_INT,status.MPI_SOURCE,step,MPI_COMM_WORLD,&request);
                     out++;
                     if (out >= size){
-                        ok = false;
+                        ok = 0;
                     }
                 }
             }
             else {
                 if (tag < size){
-                    
+                    int temp = tag * (N/size);
+                    save(A,temp,temp + (N/size),lcl);
+                    done++;
                 }
                 else if (tag >= size && tag < (size + size/2)){
-
+                    int aux = tag - size;
+                    int temp = aux * (N/size) * 2;
+                    save(A,temp, temp + (N/size) * 2, lcl);
                 }
             }
         
